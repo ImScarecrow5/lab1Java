4b95225ade24a6dc9107b87a7c0d98f1940c6bdd
Anton <94841246+ImScarecrow5@users.noreply.github.com>
2026-03-17 09:39:27 +0300

    Delete lab1Sigov directory

--- lab1Sigov/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.lab1sigov {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires jdk.jdi;
-
-
-    opens com.example.lab1sigov to javafx.fxml;
-    exports com.example.lab1sigov;
-}
